3a3c80fe7da6f4bd6a95cac27922ce99789e27b3
a314151 <li-ry24@mails.tsinghua.edu.cn>
2026-02-02 15:22:02 +0800

    更新版本号为2.0.2

--- App.tsx
+++ App.tsx
@@ -30,7 +30,7 @@ import BackButton from './components/ui/BackButton';
 import AuthModal from './components/modals/AuthModal';
 
 // --- Constants ---
-const DATA_VERSION = "2.0.1"; 
+const DATA_VERSION = "2.0.2"; 
 
 const DEFAULT_HERO: Hero = {
     id: 'default_adventurer',
